f74d2123f204dc05226d5631e070bf235a13c742
liangrongze <gz.liangrongze@gmail.com>
2013-03-30 02:05:11 +0800

    cbc theme

---
+++ addons/shared_addons/themes/cbc/theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Cbc extends Theme
+{
+    public $name			= 'CBc';
+    public $author			= 'Rongze';
+    public $author_website	= 'http://liangrongze.com/';
+    public $website			= 'http://liangrongze.com/';
+    public $description		= 'CBC website theme.';
+    public $version			= '1.0.0';
+}
+
+/* End of file theme.php */
